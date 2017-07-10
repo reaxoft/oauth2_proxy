58646b060c713a04175075c84f787bcac2a8c417
brainysmith <brainysmith@gmail.com>
2017-07-10 17:07:13 +0300

    Added BlitzIdp provider

--- providers/blitzidp.go
+++ providers/blitzidp.go
@@ -1,9 +1,52 @@
 package providers
 
 import(
-	_ "github.com/bitly/oauth2_proxy/api"
+	"errors"
+	"net/http"
+	"fmt"
+	"github.com/bitly/oauth2_proxy/api"
 )
 
 type BlitzIdpProvider struct {
 	*ProviderData
+}
+
+func NewBlitzIdpProvider(p *ProviderData) *BlitzIdpProvider {
+	p.ProviderName = "BlitzIdp"
+	return &BlitzIdpProvider{ProviderData: p}
+}
+
+func makeOAuthHeader(access_token string) http.Header {
+	header := make(http.Header)
+	header.Set("Accept", "application/json")
+	header.Set("Authorization", fmt.Sprintf("Bearer %s", access_token))
+	return header
+}
+
+func (p *BlitzIdpProvider) ValidateSessionState(s *SessionState) bool {
+	return validateToken(p, s.AccessToken, makeOAuthHeader(s.AccessToken))
+}
+
+func (p *BlitzIdpProvider) GetEmailAddress(s *SessionState) (string, error) {
+	if s.AccessToken == "" {
+		return "", errors.New("missing access token")
+	}
+	req, err := http.NewRequest("GET", p.ProfileURL.String(), nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header = makeOAuthHeader(s.AccessToken)
+
+	type result struct {
+		Email string `json:"email"`
+	}
+	var r result
+	err = api.RequestJson(req, &r)
+	if err != nil {
+		return "", err
+	}
+	if r.Email == "" {
+		return "", errors.New("no email")
+	}
+	return r.Email, nil
 }
--- providers/providers.go
+++ providers/providers.go
@@ -30,6 +30,8 @@ func New(provider string, p *ProviderData) Provider {
 		return NewAzureProvider(p)
 	case "gitlab":
 		return NewGitLabProvider(p)
+	case "blitzidp":
+		return NewBlitzIdpProvider(p)
 	default:
 		return NewGoogleProvider(p)
 	}
